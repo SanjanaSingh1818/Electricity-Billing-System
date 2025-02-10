245f28cd3ac57332d83a3829d4a9c3007200f476
SanjanaSingh1818 <sanjanasingh63072@gmail.com>
2025-02-10 13:23:15 +0530

    init comit

---
+++ src/electricity/billing/system/database.java
@@ -0,0 +1,14 @@
+package electricity.billing.system;
+import java.sql.*;
+public class database {
+    Connection connection;
+    Statement statement;
+    database(){
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Bill_system","root","@Singh11");
+            statement = connection.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
